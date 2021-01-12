ab26fec8609993f5e7c8f35467247541836cf87f
Henry <henrygu054@gmail.com>
2021-01-12 16:01:03 -0500

    email change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li> Henry Gu 2020</li>
+              <li>Made with <a title="Styleshout" href="https://reactjs.org/">React JS</a></li>
            </ul>
 
         </div>
